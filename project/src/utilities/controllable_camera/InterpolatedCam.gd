28770f304b2d4a411f1904729e90a5487c15cb28
chalvikves <vikves@student.chalmers.se>
2021-11-02 18:44:40 +0100

    this sprint

--- project/src/utilities/controllable_camera/InterpolatedCam.gd
+++ project/src/utilities/controllable_camera/InterpolatedCam.gd
@@ -6,6 +6,8 @@
 
 extends Camera
 
+class_name InterpCam
+
 export (float) var speed = 15.0 
 export (NodePath) var target = NodePath("") setget set_target
 export (bool) var enabled = true setget set_interpolation_enabled, is_interpolation_enabled
@@ -40,17 +42,4 @@ func _physics_process(delta) -> void:
 		var local_transform = get_global_transform()
 		local_transform = local_transform.interpolate_with(target_xform, nDelta)
 		set_global_transform(local_transform)
-		var cam := node as Camera
-		if (cam):
-			if (cam.get_projection() == get_projection()):
-
-				var new_near = lerp(near, cam.get_znear(), nDelta)
-				var new_far = lerp(far, cam.getzfar(), nDelta)
-
-				if (cam.get_projection() == PROJECTION_ORTHOGONAL):
-					var size = lerp(get_size(), cam.get_size(), speed * delta)
-					set_orthogonal(size, new_near, new_far)
-				else:
-					var fov = lerp(get_fov(), cam.get_fov(), speed * delta)
-					set_perspective(fov, new_near, new_far)
 	
